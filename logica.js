03f5b6ee4d905640165ebfa7c7d4c3f8f6abd8c6
David Cortijo <davidcortijotraba@gmail.com>
2020-04-24 22:21:33 +0200

    Falta borrar y filtrar

--- logica.js
+++ logica.js
@@ -1,6 +1,7 @@
 
-// Aqui creo el objeto principal de la práctica, Tarea.
-
+/*
+  Aquí creo el objeto principal de la práctica, Tarea.
+*/
 function Tarea (texto, prioridad, estado) {
     this.texto = texto;
     this.prioridad = prioridad;
@@ -30,49 +31,33 @@ function Tarea (texto, prioridad, estado) {
     }
 }
 
-
 var tareas = [];
+
 /* 
   - Index es una variable global que uso para saber en la lista de tareas, qué tarea es cada una según su posición en el array.
   - Me sirve para colocar en cada hijo de li un atributo id cuyo valor es el index (la posición que ocupa en el array).
-  - No sé si es la mejor decisión, y cuál sería mejor para saber qué tarea dentro del array es cual en la lista de tareas.
+  - No sé si es la mejor decisión, y cuál sería mejor para saber qué tarea de la lista de tareas es cual dentro del array.
   - Sea como sea tengo en mente alguna forma de hacer los filtrados por texto y las ordenaciones por prioridad y borrados siguiendo esta línea de
     la variable global index si no te parece mala idea.
 */
-var index = -1; 
-document.getElementById("boton").onclick = annadirTarea;
+var index = -1;
 
-//Este es el manejador de eventos que llamo cuando quiero crear una tarea y añadirla a la lista. Por defecto su prioridad es 3 y su estado pendiente.
-function annadirTarea() {
+document.getElementById("boton").onclick = crearTarea;
+document.getElementById("ordenPrio").onclick = ordenarPorPrioridad;
+//document.getElementById("borrarRealizadas").onclick = borrarTareasRealizadas;
+
+comprobarLista();
+
+/*
+  Este es el manejador de eventos que llamo cuando quiero crear una nueva tarea. Por defecto su prioridad es 3 y su estado pendiente.
+*/
+function crearTarea() {
     let inputTexto = document.getElementById("texto");
-
     let tarea = new Tarea(inputTexto.value, 3, "pendiente");
     tareas.push(tarea);
     index++;
-
-    let lista = document.getElementById("lista");
-
-    let li = document.createElement("li");
-    let divTarea = document.createElement("div");
-    let divPrioridad = document.createElement("div");
-    let parrafoTexto = document.createElement("p");
-
-    divTarea.setAttribute("id", index);
-    parrafoTexto.setAttribute("id", "pendiente");
-    divPrioridad.setAttribute("class", "prioridad");
-    divPrioridad.setAttribute("id", "prioridad3");
-
-    divPrioridad.onclick = manejadorPrioridad;
-    parrafoTexto.onclick = manejadorEstado;
-
-    parrafoTexto.innerHTML = inputTexto.value;
+    listarTarea(tarea, index);
     inputTexto.value = "";
-
-    divTarea.appendChild(divPrioridad);
-    divTarea.appendChild(parrafoTexto);
-    li.appendChild(divTarea);
-
-    lista.appendChild(li);
 
 }
 
@@ -90,6 +75,91 @@ function manejadorPrioridad() {
 function manejadorEstado() {
     let tarea = tareas[this.parentNode.getAttribute("id")];
     this.setAttribute("id", tarea.establecerEstado(this.getAttribute("id")));
+}
+
+
+/*
+  Esta función es la encargada de ordenar todas las tareas de la lista por su prioridad.
+*/
+function ordenarPorPrioridad() {
+
+    document.getElementById("lista").innerHTML = "";
+    tareas.sort((a, b) => {
+        if (a.prioridad > b.prioridad) {
+            return 1;
+        }
+
+        if (a.prioridad < b.prioridad) {
+            return -1;
+        }
+
+        if (a.prioridad == b.prioridad) {
+            return 0;
+        }
+    });
+    index = -1;
+    for (let tarea of tareas) {
+        index++;
+        listarTarea(tarea, index);
+    }
+
+}
+
+/*
+function borrarTareasRealizadas() {
+
+}*/
+
+
+/*
+  Esta función comprueba si la lista está vacía y si lo está, oculta los botones de ordenación y borrado para evitar errores.
+*/
+function comprobarLista() {
+    var botonesFiltrado = document.getElementsByClassName("filtrados");
+    var lista = document.getElementById("lista");
+
+    if (lista.childNodes.length == 0) {
+        for (let boton of botonesFiltrado) {
+            boton.style.display = "none";
+        }
+    } else {
+        for (let boton of botonesFiltrado) {
+            boton.style.display = "inline-block";
+        }
+    }
+}
+
+
+/*
+  Esta función añade a la lista una tarea. Es tan sólo de ayuda para no repetir código.
+*/
+function listarTarea(tarea, index) {
+
+    let lista = document.getElementById("lista");
+    let inputTexto = tarea.texto;
+    let li = document.createElement("li");
+    let divTarea = document.createElement("div");
+    let divPrioridad = document.createElement("div");
+    let parrafoTexto = document.createElement("p");
+
+    divTarea.setAttribute("id", index);
+    parrafoTexto.setAttribute("id", tarea.estado);
+    divPrioridad.setAttribute("class", "prioridad");
+    divPrioridad.setAttribute("id", "prioridad" + tarea.prioridad);
+
+    divPrioridad.onclick = manejadorPrioridad;
+    parrafoTexto.onclick = manejadorEstado;
+
+    parrafoTexto.innerHTML = inputTexto;
+
+    divTarea.appendChild(divPrioridad);
+    divTarea.appendChild(parrafoTexto);
+    li.appendChild(divTarea);
+
+    lista.appendChild(li);
+    if (index == 0) {
+        comprobarLista();
+    }
 }
 
 
